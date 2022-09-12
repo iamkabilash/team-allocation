15384c2763b1fa47a9595005ed15d2e0d5c20db7
Kabilash Satheesh Kumar <iamkabilash@Kabilashs-MacBook-Pro.local>
2022-09-12 22:25:46 +0530

    starter files

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () =>{
     return (
         <>
-            <h1>Header</h1>
+            <h1 className="text-orange-700">Header</h1>
         </>
     );
 }
